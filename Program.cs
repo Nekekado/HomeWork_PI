006907d9e744dc4642a4538e1ec2d280ac5f6a13
Nekekado <123927262+Nekekado@users.noreply.github.com>
2024-12-21 11:44:47 +0700

    Update Program.cs to Summary

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,8 +6,30 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write a:");
+            string? s = Console.ReadLine();
+
+            int a;
+            int b;
+            
+            while (!Int32.TryParse(s, out a))
+            {
+                Console.WriteLine("Wrong a!");
+                Console.WriteLine("Write a again:");
+                s = Console.ReadLine();
+            }
+                
+            Console.WriteLine("Write b:");
+            s = Console.ReadLine();
+            
+            while (!Int32.TryParse(s, out b))
+            {
+                Console.WriteLine("Wrong b!");
+                Console.WriteLine("Write b again:");
+                s = Console.ReadLine();
+            }
+            
+            Console.WriteLine($"{a} + {b} = {a + b}");
         }
     }
 }
-
